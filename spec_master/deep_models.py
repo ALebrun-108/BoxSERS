bed7ab59d1488f982fca92704a2161eeebd5f5e1
ALebrun-108 <59709058+ALebrun-108@users.noreply.github.com>
2020-06-17 16:49:00 -0400

    Import of the spec_master package and creation of a  jupyter notebook

---
+++ spec_master/deep_models.py
@@ -0,0 +1,331 @@
+import keras
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Convolution1D as Conv1D
+from keras.layers import LocallyConnected1D as Lc1D
+from keras.layers import Dense, Dropout, Flatten, MaxPooling1D, BatchNormalization, Activation
+from sklearn.metrics import classification_report, confusion_matrix
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from numpy import savetxt
+import numpy as np
+from keras.models import load_model
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+class SpectroCNN:
+
+    def __init__(self, x_train, y_train, ks=5, output_activation='softmax', architecture='Conv_Model'):
+
+        self.ks = ks  # Kernel filter size
+        # Features modifications for CNN model: shape_initial = (a,b) --> shape_final = (a,b,1)
+        self.x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))
+        self.y_train = y_train  # Training labels
+        self.callbacks = []  # callbacks for the training
+        self.output_activation = output_activation  # Output layer activation function
+        self.status = 'untrained'
+        self.arch = architecture
+
+        if self.arch == 'Conv_Model':
+            self.model = Sequential(name='Conv_Model')
+            # conv block 1
+            self.model.add(Conv1D(8, kernel_size=self.ks, input_shape=(np.shape(x_train)[1], 1),
+                                  padding='same', name='conv1'))
+            self.model.add(BatchNormalization())
+            self.model.add(Activation('relu'))
+            self.model.add(MaxPooling1D(pool_size=2, name='maxp1'))
+            # conv block 2
+            self.model.add(Conv1D(16, self.ks, padding='same', name='conv2'))
+            self.model.add(BatchNormalization())
+            self.model.add(Activation('relu'))
+            self.model.add(MaxPooling1D(pool_size=2, name='maxp2'))
+            # conv block 3
+            self.model.add(Conv1D(32, self.ks, padding='same', name='conv3'))
+            self.model.add(BatchNormalization())
+            self.model.add(Activation('relu'))
+            self.model.add(MaxPooling1D(pool_size=2, name='maxp3'))
+            # classification block
+            self.model.add(Flatten())
+            self.model.add(Dense(1024))
+            self.model.add(BatchNormalization())
+            self.model.add(Activation('relu'))
+            self.model.add(Dropout(0.5))
+            self.model.add(Dense(1024))
+            self.model.add(BatchNormalization())
+            self.model.add(Activation('relu'))
+            self.model.add(Dropout(0.5))
+            self.model.add(Dense(self.y_train.shape[1]))
+            self.model.add(Activation(self.output_activation))
+
+        elif self.arch == 'LC_Model':
+            self.model = Sequential(name='LC_Model')
+            # conv block 1
+            self.model.add(Conv1D(8, kernel_size=self.ks, input_shape=(np.shape(x_train)[1], 1),
+                                  padding='same', name='conv1'))
+            self.model.add(BatchNormalization())
+            self.model.add(Activation('relu'))
+            self.model.add(MaxPooling1D(pool_size=2, name='maxp1'))
+            # conv block 2
+            self.model.add(Conv1D(16, self.ks, padding='same', name='conv2'))
+            self.model.add(BatchNormalization())
+            self.model.add(Activation('relu'))
+            self.model.add(MaxPooling1D(pool_size=2, name='maxp2'))
+            # conv block 3
+            self.model.add(Lc1D(32, self.ks, name='LC3'))
+            self.model.add(BatchNormalization())
+            self.model.add(Activation('relu'))
+            self.model.add(MaxPooling1D(pool_size=2, name='maxp3'))
+            # classification block
+            self.model.add(Flatten())
+            self.model.add(Dense(1024))
+            self.model.add(BatchNormalization())
+            self.model.add(Activation('relu'))
+            self.model.add(Dropout(0.5))
+            self.model.add(Dense(1024))
+            self.model.add(BatchNormalization())
+            self.model.add(Activation('relu'))
+            self.model.add(Dropout(0.5))
+            self.model.add(Dense(self.y_train.shape[1]))
+            self.model.add(Activation(self.output_activation))
+        else:
+            raise ValueError('Invalid architecture, load your own model or use: '
+                             '{\'Conv_Model\',\'LC_Model\'}')
+
+    def print_model_sumary(self):
+        self.model.summary()
+
+    def config_earlystopping(self, monit='val_loss', epoch=5, verbose=1, best_weights=False):
+        self.callbacks.append(EarlyStopping(monitor=monit,
+                                            patience=epoch,
+                                            verbose=verbose,
+                                            restore_best_weights=best_weights))
+
+    def config_modelcheckpoint(self, monit='val_loss', verbose=0,  best_only=True, save_path='best_model.h5'):
+        self.callbacks.append(ModelCheckpoint(monitor=monit,
+                                              filepath=save_path,
+                                              verbose=verbose,
+                                              save_best_only=best_only))
+
+    def compile_model(self, optimizer='Adam', learning_rate=0.001, loss='categorical_crossentropy'):
+        if optimizer is 'Adam':
+            opt = keras.optimizers.Adam(lr=learning_rate)
+        elif optimizer is 'SGD':
+            opt = keras.optimizers.SGD(lr=learning_rate)
+        else:
+            raise NameError('Invalid optimizer')
+
+        self.model.compile(loss=loss, optimizer=opt, metrics=['accuracy'])
+
+    def train_model(self, x_test, y_test, b_s=64, ep=25, plot_history_enabled=False):
+
+        # Features modifications for CNN model: shape_initial = (a,b) --> shape_final = (a,b,1)
+        x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
+
+        train_history = self.model.fit(self.x_train, self.y_train, batch_size=b_s, epochs=ep, verbose=1,
+                                       shuffle=True, callbacks=self.callbacks, validation_data=(x_test, y_test))
+        if plot_history_enabled:
+            self.plot_history(train_history)
+        self.status = 'trained'
+        return train_history
+
+    @staticmethod
+    def plot_history(history):
+        plt.subplots()
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.xlabel('Epoch #')
+        plt.ylabel('Accuracy')
+        plt.ylim(0, 1)
+        plt.legend(['Train Acc', 'Val Acc'], loc='best')
+        plt.show()
+
+        plt.subplots()
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.xlabel('Epoch #')
+        plt.ylabel('Loss')
+        plt.legend(['Train loss', 'Val Loss'], loc='best')
+        plt.show()
+
+    def predict_classes(self, x_test):
+        x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
+        y_pred = self.model.predict_classes(x_test)
+        return y_pred
+
+    def predict_proba(self, x_test):
+        x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
+        y_pred = self.model.predict_proba(x_test)
+        return y_pred
+
+    def get_classif_report(self, x_test, y_test, class_names=None, save_path=None):
+        x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))  # Features modifications for CNN model
+        cnn_scores = self.model.predict_classes(x_test)  # Returns integer values corresponding to the classes
+        report = classification_report(y_test, cnn_scores, target_names=class_names, digits=6)
+
+        if save_path is not None:
+            text_file = open(save_path, "w")
+            text_file.write(report)
+            text_file.close()
+        return report
+
+    def get_conf_matrix(self, x_test, y_test, normalize='true', class_names='',  save_path=None):
+
+        x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
+        y_pred = self.model.predict_classes(x_test)
+        conf_matrix = confusion_matrix(y_test, y_pred, normalize=normalize)
+        if save_path is not None:
+            comment = 'Classes in order:'+str(class_names) + '\n Row = True classes, Col = Predicted classes'
+            savetxt(save_path, conf_matrix,
+                    delimiter=',',
+                    fmt='%1.3f',
+                    header=comment)
+        return conf_matrix
+
+    def features_extractor(self, x_test, layer_name):
+        x_test = np.array(x_test, ndmin=2)
+        x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
+        layer = self.model.get_layer(name=layer_name)
+        keras_function = keras.backend.function(self.model.input, layer.output)
+        features = keras_function([x_test])
+        return features
+
+    def current_status(self):
+        return self.status
+
+    def save_model(self, save_path):
+        self.model.save(save_path)
+
+    def load_model(self, model_path):
+        self.model = load_model(model_path)
+
+
+class SpectroDNN:
+
+    def __init__(self, x_train, y_train, output_activation='softmax'):
+
+        # Features modifications for CNN model: shape_initial = (a,b) --> shape_final = (a,b,1)
+        self.x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))
+        self.y_train = y_train  # Training labels
+        self.callbacks = []  # callbacks for the training
+        self.output_activation = output_activation  # Output layer activation function
+        self.status = 'untrained'
+
+        self.model = Sequential(name='Dense Model')
+        # conv block 1
+        self.model.add(Dense(1024, input_shape=(np.shape(x_train)[1], 1), name='dense1'))
+        self.model.add(BatchNormalization())
+        self.model.add(Activation('relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(1024))
+        self.model.add(BatchNormalization())
+        self.model.add(Activation('relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(self.y_train.shape[1]))
+        self.model.add(Activation(self.output_activation))
+
+    def print_model_sumary(self):
+        self.model.summary()
+
+    def config_earlystopping(self, monit='val_loss', epoch=5, verbose=1, best_weights=False):
+        self.callbacks.append(EarlyStopping(monitor=monit,
+                                            patience=epoch,
+                                            verbose=verbose,
+                                            restore_best_weights=best_weights))
+
+    def config_modelcheckpoint(self, monit='val_loss', verbose=0,  best_only=True, save_path='best_model.h5'):
+        self.callbacks.append(ModelCheckpoint(monitor=monit,
+                                              filepath=save_path,
+                                              verbose=verbose,
+                                              save_best_only=best_only))
+
+    def compile_model(self, optimizer='Adam', learning_rate=0.001, loss='categorical_crossentropy'):
+        if optimizer is 'Adam':
+            opt = keras.optimizers.Adam(lr=learning_rate)
+        elif optimizer is 'SGD':
+            opt = keras.optimizers.SGD(lr=learning_rate)
+        else:
+            raise NameError('Invalid optimizer')
+
+        self.model.compile(loss=loss, optimizer=opt, metrics=['accuracy'])
+
+    def train_model(self, x_test, y_test, b_s=64, ep=25, plot_history_enabled=False):
+
+        # Features modifications for CNN model: shape_initial = (a,b) --> shape_final = (a,b,1)
+        x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
+
+        train_history = self.model.fit(self.x_train, self.y_train, batch_size=b_s, epochs=ep, verbose=1,
+                                       shuffle=True, callbacks=self.callbacks, validation_data=(x_test, y_test))
+        if plot_history_enabled:
+            self.plot_history(train_history)
+        self.status = 'trained'
+        return train_history
+
+    @staticmethod
+    def plot_history(history):
+        plt.subplots()
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.xlabel('Epoch #')
+        plt.ylabel('Accuracy')
+        plt.ylim(0, 1)
+        plt.legend(['Train Acc', 'Val Acc'], loc='best')
+        plt.show()
+
+        plt.subplots()
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.xlabel('Epoch #')
+        plt.ylabel('Loss')
+        plt.legend(['Train loss', 'Val Loss'], loc='best')
+        plt.show()
+
+    def predict_classes(self, x_test):
+        x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
+        y_pred = self.model.predict_classes(x_test)
+        return y_pred
+
+    def predict_proba(self, x_test):
+        x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
+        y_pred = self.model.predict_proba(x_test)
+        return y_pred
+
+    def get_classif_report(self, x_test, y_test, class_names=None, save_path=None):
+        x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))  # Features modifications for CNN model
+        cnn_scores = self.model.predict_classes(x_test)  # Returns integer values corresponding to the classes
+        report = classification_report(y_test, cnn_scores, target_names=class_names, digits=6)
+
+        if save_path is not None:
+            text_file = open(save_path, "w")
+            text_file.write(report)
+            text_file.close()
+        return report
+
+    def get_conf_matrix(self, x_test, y_test, normalize='true', class_names='',  save_path=None):
+
+        x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
+        y_pred = self.model.predict_classes(x_test)
+        conf_matrix = confusion_matrix(y_test, y_pred, normalize=normalize)
+        if save_path is not None:
+            comment = 'Classes in order:'+str(class_names) + '\n Row = True classes, Col = Predicted classes'
+            savetxt(save_path, conf_matrix,
+                    delimiter=',',
+                    fmt='%1.3f',
+                    header=comment)
+        return conf_matrix
+
+    def features_extractor(self, x_test, layer_name):
+        x_test = np.array(x_test, ndmin=2)
+        x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
+        layer = self.model.get_layer(name=layer_name)
+        keras_function = keras.backend.function(self.model.input, layer.output)
+        features = keras_function([x_test])
+        return features
+
+    def current_status(self):
+        return self.status
+
+    def save_model(self, save_path):
+        self.model.save(save_path)
+
+    def load_model(self, model_path):
+        self.model = load_model(model_path)
---
+++ spec_master/spectro_augmentation.py
@@ -0,0 +1,357 @@
+import numpy as np
+from sklearn.utils import shuffle
+
+
+class SpectralDataAug:
+
+    def __init__(self, sp, lab):
+        """
+        Parameters:
+            sp : Input spectrum or spectrum array(spectra/row).
+            lab : Label associated to sp.
+
+        Notes:
+            Self.sp_aug and self.lab_aug contain the newly generated and  initial spectra.
+
+            It is from self.sp and self.lab that the new spectra are generated.
+        """
+        # sp is forced to be a two-dimensional array
+        self.sp = np.array(sp, ndmin=2)
+
+        if lab.ndim == 1:  # lab is a tuple
+            # lab is converted to a column vector shape = [x,1]
+            self.lab = np.reshape(lab, (lab.shape[0], 1))
+        else:
+            self.lab = lab
+
+        self.sp_aug = self.sp
+        self.lab_aug = self.lab
+
+    def out(self):
+        return self.sp_aug, self.lab_aug
+
+    def base_sizes(self):
+        return self.sp.shape, self.lab.shape
+
+    def aug_sizes(self):
+        return self.sp_aug.shape, self.lab_aug.shape
+
+    def updtate_base(self, new_sp=None, new_lab=None):
+
+        if new_sp and new_lab is None:
+            print('ss')
+
+        self.sp = self.sp_aug
+        self.lab = self.lab_aug
+
+    @staticmethod
+    def noise_addition(sp, noise_std):
+        """
+        Add gaussian noise
+
+        Parameters:
+            sp: Input spectrum or spectrum array(spectra/row).
+            noise_std(int or float): Noise standard deviation.
+
+        Returns:
+            (ndarray): Spectrum or spectra with noise added
+        """
+        return np.random.normal(sp, noise_std)
+
+    def aug_noise(self, noise_std, iterations=1, update=False):
+        """
+        Description
+
+        Parameters:
+            noise_std:
+            iterations(int):
+            noise_std(float or None):
+            update(bool):
+
+        Return:
+            (ndarray, ndarray): Augmented spectrum and label arrays
+        """
+        for i in range(iterations):
+            # generation of new spectra
+            noise_sp = self.noise_addition(self.sp, noise_std)
+            # new spectra & labels are appended to the original ones
+            self.sp_aug = np.vstack((self.sp_aug, noise_sp))
+            self.lab_aug = np.vstack((self.lab_aug, self.lab))
+        # the spectra and labels are randoml ymixed
+        self.sp_aug, self.lab_aug = shuffle(self.sp_aug, self.lab_aug)
+        if update:
+            # overwrites source data with augmented data
+            self.sp = self.sp_aug
+            self.lab = self.lab_aug
+
+        return self.sp_aug, self.lab_aug
+
+    @staticmethod
+    def ioffset(sp, offset):
+        """
+        Adds a uniform intensity offset to the signal
+
+        Parameters:
+            sp: Input spectrum or spectrum array(spectra/row).
+            offset(int or float): Offset
+
+        Returns:
+            (ndarray): Spectrum or spectra with offset added
+        """
+        return sp + offset
+
+    def aug_ioffset(self, offset_lim, iterations=1, noise_std=None, update=False):
+        """
+        Description
+
+        Parameters:
+            offset_lim:
+            iterations(int):
+            noise_std(float or None):
+            update(bool):
+
+        Return:
+            (ndarray, ndarray): Augmented spectrum and label arrays
+        """
+        for i in range(iterations):
+            # random intensity offset(s) generation using uniform distribution
+            arr_offset = np.random.uniform(-offset_lim, offset_lim, (self.sp.shape[0], 1))
+            # generation of new spectra
+            offset_sp = self.ioffset(self.sp, arr_offset)
+            if noise_std is not None:
+                # noise is added on the new spectra
+                offset_sp = self.noise_addition(offset_sp, noise_std)
+
+            # new spectra & labels are appended to the original ones
+            self.sp_aug = np.vstack((self.sp_aug, offset_sp))
+            self.lab_aug = np.vstack((self.lab_aug, self.lab))
+
+        # the spectra and labels are randomly mixed
+
+        self.sp_aug, self.lab_aug = shuffle(self.sp_aug, self.lab_aug)
+        if update:
+            # overwrites source data with augmented data
+            self.sp = self.sp_aug
+            self.lab = self.lab_aug
+
+        return self.sp_aug, self.lab_aug
+
+    @staticmethod
+    def wshift(sp, w_shft, fill_mode='edge', value=0):
+        """
+        Moves spectrum/spectra along the wavelength/Raman shift  axis.
+
+        Parameters:
+            sp: Input spectrum or spectrum array(spectra/row).
+            w_shft: wavelenth or wavenumber shift.
+            fill_mode(str): {'edge', 'fixed'} Filling mode used for new values created when shifting spectra.
+                'edge': Edge values are used to fill new values
+                'fixed': A fixed value(set_value) is used to fill new values
+            value: Filling value used when the filling mode is 'constant'
+
+        Returns:
+            ndarray: Spectrum/spectra following the shift(s) on the wavelength axis.
+
+        Notes:
+            w_shft must either be a dimensionless array or a column vector and its size must not exceed
+            the number of spectra contained in sp.
+        """
+
+        w_shft = np.array(w_shft, ndmin=2)
+        # sp_out initialization
+        sp = np.array(sp, ndmin=2)
+        shft_sp = sp
+        # fill value initialization
+        fill_value_left = np.zeros_like(w_shft)
+        fill_value_right = np.zeros_like(w_shft)
+        # w_shft index splitting for positive and negative shifts
+        positives = np.argwhere(w_shft > 0)[:, 0]
+        negatives = np.argwhere(w_shft < 0)[:, 0]
+
+        if fill_mode == 'edge':
+            fill_value_left = sp[:, 0]
+            fill_value_right = sp[:, -1]
+        elif fill_mode == 'fixed':
+            fill_value_left[:] = value
+            fill_value_right[:] = value
+        else:
+            print('Invalid fill_mode: fill value(s) has been set to zero')
+
+        for p in positives:
+            shft_sp[p, :int(w_shft[p])] = fill_value_left[p]
+            shft_sp[p, int(w_shft[p]):] = sp[p, :-int(w_shft[p])]
+        for n in negatives:
+            shft_sp[n, int(w_shft[n]):] = fill_value_right[n]
+            shft_sp[n, :int(w_shft[n])] = sp[n, -int(w_shft[n]):]
+        return shft_sp
+
+    def aug_wshift(self, w_shft_lim, iterations=1, noise_std=None, update=False):
+        """
+        Description
+
+        Parameters:
+            w_shft_lim:
+            iterations(int):
+            noise_std(float or None):
+            update(bool):
+
+        Return:
+            (ndarray, ndarray): Augmented spectrum and label arrays
+        """
+
+        for i in range(iterations):
+            # random shift(s) generation using uniform distribution
+            arr_shift = np.random.randint(-w_shft_lim, w_shft_lim, (self.sp.shape[0], 1))
+            # generation of new spectra
+            shift_sp = self.wshift(self.sp, arr_shift)
+            if noise_std is not None:
+                # noise is added on the new spectra
+                shift_sp = self.noise_addition(shift_sp, noise_std)
+
+            # new spectra & labels are appended to the original ones
+            self.sp_aug = np.vstack((self.sp_aug, shift_sp))
+            self.lab_aug = np.vstack((self.lab_aug, self.lab))
+
+        # the spectra and labels are randomly mixed
+        self.sp_aug, self.lab_aug = shuffle(self.sp_aug, self.lab_aug)
+        if update:
+            # overwrites source data with augmented data
+            self.sp = self.sp_aug
+            self.lab = self.lab_aug
+
+        return self.sp_aug, self.lab_aug
+
+    @staticmethod
+    def linear_slope(sp, slope, inter):
+        """
+        Adds a linear slope to the signal
+
+        Parameters:
+            sp: Input spectrum or spectrum array(spectra/row).
+            slope(int or float): Linear slope slope parameter.
+            inter(int or float): Linear slope intercept parameter.
+
+        Returns:
+            (ndarray): Spectrum or spectra with linear slope added
+        """
+        sp = np.array(sp, ndmin=2)
+        slope_range = np.arange(-(sp.shape[1]) / 2, (sp.shape[1]) / 2)
+        return sp + (slope * slope_range + inter)
+
+    def aug_linear_slope(self, slope_lim, inter_lim, iterations=1, noise_std=None, update=False):
+        """
+        Description
+
+        Parameters:
+            slope_lim:
+            inter_lim:
+            iterations(int):
+            noise_std(float):
+            update(bool):
+
+        Return:
+            (ndarray, ndarray): Augmented spectrum and label arrays
+        """
+        for i in range(iterations):
+            # random slope and intercept(s) generation using uniform distribution
+            arr_slope = np.random.uniform(-slope_lim, slope_lim, (self.sp.shape[0], 1))
+            arr_inter = np.random.uniform(-inter_lim, inter_lim, (self.sp.shape[0], 1))
+            # generation of new spectra
+            slope_sp = self.linear_slope(self.sp, arr_slope, arr_inter)
+            if noise_std is not None:
+                # noise is added on the new spectra
+                slope_sp = self.noise_addition(slope_sp, noise_std)
+
+            # new spectra & labels are appended to the original ones
+            self.sp_aug = np.vstack((self.sp_aug, slope_sp))
+            self.lab_aug = np.vstack((self.lab_aug, self.lab))
+
+        # the spectra and labels are randomly mixed
+        self.sp_aug, self.lab_aug = shuffle(self.sp_aug, self.lab_aug)
+        if update:
+            # overwrites source data with augmented data
+            self.sp = self.sp_aug
+            self.lab = self.lab_aug
+
+        return self.sp_aug, self.lab_aug
+
+    @staticmethod
+    def multiplier(sp, multiplier):
+        return sp * multiplier
+
+    def aug_multiplier(self, mult_lim=0.1, iterations=1, noise_std=None, update=False):
+        """
+        Description
+
+        Parameters:
+            mult_lim:
+            iterations(int):
+            noise_std(float):
+            update(bool):
+
+        Return:
+            (ndarray, ndarray): Augmented spectrum and label arrays
+        """
+        for i in range(iterations):
+            # random multiplier(s) generation using uniform distribution
+            arr_mult = np.random.uniform(1 - mult_lim, 1 + mult_lim, (self.sp.shape[0], 1))
+            # generation of new spectra
+            mult_sp = self.multiplier(self.sp, arr_mult)
+            if noise_std is not None:
+                # noise is added on the new spectra
+                mult_sp = self.noise_addition(mult_sp, noise_std)
+            # new spectra & labels are appended to the original ones
+            self.sp_aug = np.vstack((self.sp_aug, mult_sp))
+            self.lab_aug = np.vstack((self.lab_aug, self.lab))
+
+        # the spectra and labels are randomly mixed
+        self.sp_aug, self.lab_aug = shuffle(self.sp_aug, self.lab_aug)
+        if update:
+            # overwrites source data with augmented data
+            self.sp = self.sp_aug
+            self.lab = self.lab_aug
+
+        return self.sp_aug, self.lab_aug
+
+    def aug_mixup(self, n_spec=2, alpha=0.5, iterations=1, update=False):
+        """
+        Spectrum linear combinaison process inspired by the Mixup method (Zhang, Hongyi, et al. 2017).
+
+        Parameters:
+            n_spec(int): Amount of spectrum mixed together.
+            alpha(float): Dirichlet distribution concentration parameter.
+            iterations(int): Number of iterations.
+            update(bool):
+
+        Returns:
+            (ndarray, ndarray): Augmented spectrum and label arrays
+        """
+        # initialization and space allocation
+        alpha_array = np.ones(n_spec) * alpha
+        # Lambda values generated with a dirichlet distribution
+        lam_s = np.random.dirichlet(alpha_array, self.sp.shape[0])
+
+        for i in range(iterations):
+            sp_sum = np.zeros(self.sp.shape)
+            lab_sum = np.zeros(self.lab.shape)
+            for n in range(0, n_spec):
+                arr_, lab_ = shuffle(self.sp, self.lab)
+                lam = np.array(lam_s[:, n], ndmin=2).transpose()
+                sp_sum = sp_sum + lam * arr_
+                lab_sum = lab_sum + lam * lab_
+
+            self.sp_aug = np.vstack((self.sp_aug, sp_sum))
+            self.lab_aug = np.vstack((self.lab_aug, lab_sum))
+
+        # the spectra and labels are randomly mixed
+        self.sp_aug, self.lab_aug = shuffle(self.sp_aug, self.lab_aug)
+        if update:
+            # overwrites source data with augmented data
+            self.sp = self.sp_aug
+            self.lab = self.lab_aug
+
+        return self.sp_aug, self.lab_aug
+
+
+if __name__ == "__main__":
+    print(help(SpectralDataAug))
