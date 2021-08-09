90cf2bfc5afe5b760e3b29e610147512dea1fda2
Alexis Lebrun <59709058+ALebrun-108@users.noreply.github.com>
2021-08-09 15:36:03 -0400

    Delete specmaster directory

--- specmaster/machine_learning/__init__.py
+++
@@ -1,3 +0,0 @@
-from specmaster.machine_learning.classic_models import SpectroRF
-from specmaster.machine_learning.neural_networks import SpectroCNN
-from specmaster.machine_learning.unsupervised_models import SpectroGmixture, SpectroKmeans
