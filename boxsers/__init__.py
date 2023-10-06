7d0cdc46d2a87c84ce7ceee11c978b2a66a05943
Alexis Lebrun <59709058+ALebrun-108@users.noreply.github.com>
2023-10-06 17:01:53 -0400

    updated init

--- boxsers/__init__.py
+++ boxsers/__init__.py
@@ -1,9 +1,12 @@
 from boxsers.machine_learning import SpectroGmixture, SpectroKmeans, SpectroRF, SpectroSVM, \
      SpectroLDA, SpectroPCA, SpectroCNN, validation_metrics
-from boxsers.data_augmentation import aug_mixup, aug_xshift, aug_noise, aug_multiplier, aug_linslope, aug_offset
+from boxsers.data_augmentation import aug_mixup, aug_xshift, aug_noise, aug_multiplier, aug_linslope, \
+    aug_offset, aug_newband
 from boxsers.preprocessing import als_baseline_cor, savgol_smoothing, spectral_cut, spline_interpolation, \
     spectral_normalization, cosmic_filter
-from boxsers.misc_tools import data_split, load_rruff, ramanshift_converter, wavelength_converter
+from boxsers.misc_tools import data_split, load_rruff, ramanshift_converter, wavelength_converter, \
+    find_classes_index, remove_classes, split_by_classes, spectro_subsampling
+
 from boxsers.visual_tools import random_plot, spectro_plot, class_plot, distribution_plot
 
 
