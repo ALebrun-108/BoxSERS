c1971d5a898695832786ef03b45472857df45db4
Alexis Lebrun <59709058+ALebrun-108@users.noreply.github.com>
2024-05-31 09:36:04 -0400

    New version (1.5.1)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
                       'tables', 'scikit-image'],
     python_requires='>=3.6',
     # *strongly* suggested for sharing
-    version='1.5.0',
+    version='1.5.1',
     # The license can be anything you like
     license='MIT',
     description='Python package that provides a full range of functionality to process and analyze vibrational'
